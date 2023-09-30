fa7ad04cdbee2150e1facd72538bf2146e90c2c8
niekky <trungkiencfc@gmail.com>
2023-09-29 22:43:53 -0700

    Setup Init folder

---
+++ app/terminal/rev0/build/stm32f1xx_hal_flash_ex.d
@@ -0,0 +1,46 @@
+build/stm32f1xx_hal_flash_ex.o: \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../../../init/rev0/config/Inc/stm32f1xx_hal_conf.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ ../../../auto/rev0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ ../../../auto/rev0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ ../../../auto/rev0/Drivers/CMSIS/Include/core_cm3.h \
+ ../../../auto/rev0/Drivers/CMSIS/Include/cmsis_version.h \
+ ../../../auto/rev0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../../auto/rev0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../../auto/rev0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+../../../init/rev0/config/Inc/stm32f1xx_hal_conf.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+../../../auto/rev0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+../../../auto/rev0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+../../../auto/rev0/Drivers/CMSIS/Include/core_cm3.h:
+../../../auto/rev0/Drivers/CMSIS/Include/cmsis_version.h:
+../../../auto/rev0/Drivers/CMSIS/Include/cmsis_compiler.h:
+../../../auto/rev0/Drivers/CMSIS/Include/cmsis_gcc.h:
+../../../auto/rev0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+../../../auto/rev0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
